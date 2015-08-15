ba544fdeb434414d370326520bb451894fdffe58
Andrius Bubelis <abube002@fiu.edu>
2015-08-15 17:35:20 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 
 namespace HelloFlexOffer
 {
-    class Program
+    class FizzBuzz
     {
-        
+
         static void Main(string[] args)
         {
-            // I assume that object could be anything, not only numeric, but could be castable into or not e.g "5" is string by castable to numeric 
-            ArrayList list =  new ArrayList{"0", 2 ,"flex", "44", "offer", 15, 5.5, 11, 22, "dog" };
+            // I assume that object could be anything, not only numeric e.g string "5" is castable to numeric, "dog" is not
+            ArrayList list = new ArrayList { "0", 2, "flex", new AnyObject() , "44", "offer", 15, 5.5, "5.5", 11, 22, "dog"};
 
             double lo = 2;
             double hi = 5.5;
@@ -60,12 +60,18 @@ namespace HelloFlexOffer
             {
                 double result = numerator / denominator;
 
-                summary.Add("Divided: " + numerator.ToString() + " by " + denominator.ToString() );
+                summary.Add("Divided: " + numerator.ToString() + " by " + denominator.ToString()); // record each valid division 
 
                 return (result == Convert.ToInt64(result));  // if result has not decimal points i.e. rounded value stays the same
 
             }
         }
 
+    }
+
+    class AnyObject
+    {
+        public int MyProperty1 { get; set; }
+        public int MyProperty2 { get; set; }
     }
 }
